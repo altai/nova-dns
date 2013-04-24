a202c745a618856d91b8acc0e76a0a5b6c0d6f33
Ivan A. Melnikov <imelnikov@griddynamics.com>
2013-04-24 09:53:25 +0400

    Add utility to sync DNS database with actual state
    Utility makes call to corresponding nova-dns api method.
    
    Issue: altai-127
    Change-Id: Ife1b74dbbd400eb5a4f81795d6cd3b38073ee4f9

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(name='nova-dns',
       author_email='openstack@griddynamics.com',
       url='http://www.griddynamics.com/openstack',
       packages=find_packages(exclude=['bin', 'smoketests', 'tests']),
-      scripts=['bin/nova-dns'],
+      scripts=['bin/nova-dns', 'bin/nova-dns-sync'],
       py_modules=[],
       test_suite='tests'
 )
